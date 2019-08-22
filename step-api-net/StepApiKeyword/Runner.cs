47e6af80882720bd5d1364debe1cb1bb761df676
Jerome Brongniart <jerome.brongniart@exense.ch>
2019-08-22 15:36:04 +0200

    refactor tests for .Net

--- step-api-net/StepApiKeyword/Runner.cs
+++ step-api-net/StepApiKeyword/Runner.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Step.Functions.IO;
 using System;
@@ -68,77 +67,6 @@ namespace Step.Handlers.NetHandler
         public static ExecutionContext GetExecutionContext(Dictionary<string, string> properties, Type keywordClass)
         {
             return new ExecutionContext(keywordClass, properties, true);
-        }
-    }
-
-    [TestClass]
-    public class ScriptRunnerTest
-    {
-        private class TestKeywords : AbstractKeyword
-        {
-            public override bool OnError(Exception e)
-            {
-                Output.Add("onError", "true");
-                return false;
-            }
-
-            [Keyword(Name = "My Keyword")]
-            public void MyKeyword()
-            {
-                Output.Add("key", "value");
-                foreach (string key in Properties.Keys)
-                {
-                    Output.Add(key, Properties[key]);
-                }
-                Output.Add("input", (Input == null).ToString());
-                var inputMap = Input.ToObject<Dictionary<string, string>>();
-                foreach (string key in inputMap.Keys)
-                {
-                    Output.Add(key, inputMap[key]);
-                }
-            }
-
-            [Keyword(Name = "My Error Keyword")]
-            public void MyErrorKeyword()
-            {
-                throw new Exception("Test");
-            }
-
-        }
-        [TestMethod]
-        public void TestScriptRunnerOnError()
-        {
-            ExecutionContext runner = KeywordRunner.GetExecutionContext(typeof(TestKeywords));
-            var output = runner.Run("My Error Keyword", @"{}");
-
-            Assert.AreEqual("true", output.Payload["onError"]);
-        }
-
-        [TestMethod]
-        public void TestScriptRunnerRun()
-        {
-            ExecutionContext runner = KeywordRunner.GetExecutionContext(typeof(TestKeywords));
-            var output = runner.Run("My Keyword", @"{}");
-
-            Assert.AreEqual(null, output.Error);
-            Assert.AreEqual("value", output.Payload["key"]);
-        }
-
-        [TestMethod]
-        public void TestScriptRunnerWithInputsAndPropertiesRun()
-        {
-            Dictionary<string, string> properties = new Dictionary<string, string>
-            {
-                ["myProp1"] = "myValue1"
-            };
-
-            ExecutionContext runner = KeywordRunner.GetExecutionContext(typeof(TestKeywords));
-            var output = runner.Run("My Keyword", @"{'myInput1':'myInputValue1'}", properties);
-
-            Assert.AreEqual(null, output.Error);
-            Assert.AreEqual("value", output.Payload["key"]);
-            Assert.AreEqual("myValue1", output.Payload["myProp1"]);
-            Assert.AreEqual("myInputValue1", output.Payload["myInput1"]);
         }
     }
 }
