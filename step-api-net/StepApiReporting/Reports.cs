d02f238b90837d70b493cd1994b6226fc4509d66
Jerome Brongniart <jerome.brongniart@exense.ch>
2021-05-27 15:26:50 +0200

    Adding json annotation to Error

--- step-api-net/StepApiReporting/Reports.cs
+++ step-api-net/StepApiReporting/Reports.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Step.Core.Reports
 {
@@ -28,6 +29,7 @@ namespace Step.Core.Reports
         public Error(ErrorType type, string message, int code) : this(type, null, message, code, true)
         { }
 
+        [JsonConstructor]
         public Error(ErrorType type, string layer, string msg, int code, bool root)
         {
             this.type = type;
