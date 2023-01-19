e22c11522d2decaefbc95afa4dd88cba61bd9bd8
Jerome Brongniart <jerome.brongniart@exense.ch>
2023-01-19 15:12:30 +0100

    SED-621 adding dotnet unit tests

--- step-api-net/KeywordsForTesting/Keywords.cs
+++ step-api-net/KeywordsForTesting/Keywords.cs
@@ -9,5 +9,22 @@ namespace KeywordsForTesting
         {
             // We need a separate Assembly for testing "TestScriptRunnerMultipleKeywords"
         }
+        public override void BeforeKeyword(Keyword annotation)
+        {
+            output.Add("BeforeKeyword", annotation.name);
+        }
+
+        public override void AfterKeyword(Keyword annotation)
+        {
+            output.Add("AfterKeyword", annotation.name);
+        }
+    }
+    public class TestNotAbstractKeywords
+    {
+        [Keyword(name = "Not Abstract Keyword")]
+        public void NotAbstractKeyword()
+        {
+            // We need a separate Assembly for testing "TestScriptRunnerMultipleKeywords"
+        }
     }
 }
